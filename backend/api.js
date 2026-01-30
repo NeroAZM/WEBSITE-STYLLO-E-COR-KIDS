3bb79b70dc2e00e7491c4d7f6374d507b463b0d4
NeroAZM <161223249+NeroAZM@users.noreply.github.com>
2026-01-29 22:39:25 -0300

    Update api.js

--- backend/api.js
+++ backend/api.js
@@ -6,7 +6,8 @@ const serverless = require("serverless-http");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // --- CONEXÃO COM O BANCO ---
 let isConnected = false;
